3edfdfe3f5724b4377828fb95a5bc8b7868a5f1f
Fang Hau <cozyazure@hotmail.com>
2017-01-30 22:21:46 +0800

    done with create edit alter states

--- client/app/main/createedit/createeditCtrl.js
+++ client/app/main/createedit/createeditCtrl.js
@@ -3,5 +3,7 @@
     angular.module('inventory-manager')
         .controller('createeditCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
             console.log('hello',$stateParams);
+            $scope.product = $stateParams.createeditData;
+            $scope.createEditTitle = $scope.product === null ? 'Create New Product' : 'Edit Product'; 
         }])
 })();
